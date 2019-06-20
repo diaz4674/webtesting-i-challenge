18726b53b77620d4c9443a190941d322b2ffc398
Miguel <diaz4674@gmail.com>
2019-06-19 21:49:20 -0700

    installed Jest, and repair test working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("enhancer.js", () => {
+    describe("repair", () => {
+        it("restores durability to 100", () =>{
+            expect(enhancer.repair({durability: 89}).durability).toBe(100)
+        })
+    })
+})
